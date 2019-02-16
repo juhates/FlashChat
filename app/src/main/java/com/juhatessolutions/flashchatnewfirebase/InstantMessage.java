be19e5b1105e334292847e0780eef74f9e4c3fb5
juha <juhates@gmail.com>
2019-02-16 22:34:37 +0200

    first push

---
+++ app/src/main/java/com/juhatessolutions/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,24 @@
+package com.juhatessolutions.flashchatnewfirebase;
+
+class InstantMessage {
+
+    private String message;
+    private String author;
+
+    InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
